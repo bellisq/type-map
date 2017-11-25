889e7a7de84b0e06cbd66a1d7bf9e43409f728c0
4kizuki <akizuki.c10.l65@gmail.com>
2017-11-25 09:55:47 +0900

    Rename classes

--- tests/TXTypeMapAggregateTest.php
+++ tests/TXTypeMapAggregateTest.php
@@ -8,14 +8,14 @@ use Bellisq\TypeMap\TypeMapAggregate;
 use PHPUnit\Framework\TestCase;
 
 
-class TypeMapAggregateTest extends TestCase
+class TXTypeMapAggregateTest extends TestCase
 {
 
     public function testGet()
     {
         $A = new TypeMapAggregate();
-        $B = new ExampleInstantiator1();
-        $C = new ExampleInstantiator2();
+        $B = new TXInstantiatorMock();
+        $C = new TXContainerMock();
         $t = new TypeMapAggregate($A, $B, $C);
         $this->assertEquals('hello', $t->get('something'));
         $this->assertEquals('bar', $t->get('foo'));
@@ -23,14 +23,14 @@ class TypeMapAggregateTest extends TestCase
 
     public function testObjectNotFoundException()
     {
-        $t = new TypeMapAggregate((new TypeMapAggregate()), (new ExampleInstantiator1()));
+        $t = new TypeMapAggregate((new TypeMapAggregate()), (new TXInstantiatorMock()));
         $this->expectException(ObjectNotFoundException::class);
         $t->get(TypeMapAggregate::class);
     }
 
     public function testTooManyCandidatesException()
     {
-        $t = new TypeMapAggregate(new ExampleInstantiator1(), new ExampleInstantiator1());
+        $t = new TypeMapAggregate(new TXInstantiatorMock(), new TXInstantiatorMock());
         $this->expectException(TooManyCandidatesException::class);
         $t->get('something');
     }
@@ -40,17 +40,17 @@ class TypeMapAggregateTest extends TestCase
         $t = new TypeMapAggregate();
         $this->assertFalse($t->has('something'));
         $this->assertFalse($t->has('wrong'));
-        $t = new TypeMapAggregate(new ExampleInstantiator1());
+        $t = new TypeMapAggregate(new TXInstantiatorMock());
         $this->assertTrue($t->has('something'));
         $this->assertFalse($t->has('wrong'));
-        $t = new TypeMapAggregate(new ExampleInstantiator1(), new ExampleInstantiator1());
+        $t = new TypeMapAggregate(new TXInstantiatorMock(), new TXInstantiatorMock());
         $this->assertFalse($t->has('something'));
         $this->assertFalse($t->has('wrong'));
-        $t = new TypeMapAggregate(new ExampleInstantiator1(), new ExampleInstantiator2());
+        $t = new TypeMapAggregate(new TXInstantiatorMock(), new TXContainerMock());
         $this->assertTrue($t->has('something'));
         $this->assertTrue($t->has('foo'));
         $this->assertFalse($t->has('wrong'));
-        $A = new ExampleInstantiator1();
+        $A = new TXInstantiatorMock();
         $t = new TypeMapAggregate($A, $A);
         $this->assertFalse($t->has('something'));
         $this->assertFalse($t->has('wrong'));
