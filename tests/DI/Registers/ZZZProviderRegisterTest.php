fc4505dad5f16e9d516bdedfe215fdbe9904c26c
4kizuki <akizuki.c10.l65@gmail.com>
2017-12-02 14:00:54 +0900

    Update ProviderRegister

--- tests/DI/Registers/ZZZProviderRegisterTest.php
+++ tests/DI/Registers/ZZZProviderRegisterTest.php
@@ -3,8 +3,11 @@
 namespace Bellisq\TypeMap\Tests\DI\Registers;
 
 use PHPUnit\Framework\TestCase;
+use Strict\Validator\General\AlwaysPassValidator;
+use Strict\Validator\General\AlwaysFailValidator;
 use Bellisq\TypeMap\DI\Registers\ProviderRegister;
 use Bellisq\TypeMap\DI\Registers\ProviderRegisterDataTransport;
+use Bellisq\TypeMap\Exceptions\InvalidProviderException;
 
 
 class ZZZProviderRegisterTest extends TestCase
@@ -13,9 +16,16 @@ class ZZZProviderRegisterTest extends TestCase
     public function testBehavior()
     {
         $prdt = new ProviderRegisterDataTransport;
-        $pr   = new ProviderRegister($prdt);
+        $pr   = new ProviderRegister($prdt, new AlwaysPassValidator);
         $pr->register('a')->register('b');
         $this->assertEquals(['a', 'b'], $prdt->get());
+    }
+
+    public function testValidation()
+    {
+        $this->expectException(InvalidProviderException::class);
+        $pr = new ProviderRegister(new ProviderRegisterDataTransport, new AlwaysFailValidator);
+        $pr->register('a');
     }
 
 }
