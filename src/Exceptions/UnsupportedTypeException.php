1b21d1f8b25f8fb0c95e226920ba0d1d34bb5bfb
4kizuki <akizuki.c10.l65@gmail.com>
2017-12-05 08:00:47 +0900

    Add phpDoc for UnsupportedTypeException; fix #9

--- src/Exceptions/UnsupportedTypeException.php
+++ src/Exceptions/UnsupportedTypeException.php
@@ -5,6 +5,17 @@ namespace Bellisq\TypeMap\Exceptions;
 use Bellisq\TypeMap\Exceptions\TypeMapException;
 
 
+/**
+ * [ Exception ] Unsupported Type
+ * 
+ * This exception will be thrown
+ * when an instantiator does not support instantiation of the type required.
+ *
+ * @author 4kizuki <akizuki.c10.l65@gmail.com>
+ * @copyright 2017 Bellisq. All Rights Reserved.
+ * @package bellisq/type-map
+ * @since 1.0.0
+ */
 class UnsupportedTypeException extends TypeMapException
 {
 
