f3fcaf7041809e6a21728217802007f3a68efc36
4kizuki <akizuki.c10.l65@gmail.com>
2017-12-05 07:41:17 +0900

    Add phpDoc for ProviderRegisterDataTransport

--- src/DI/Registers/ProviderRegisterDataTransport.php
+++ src/DI/Registers/ProviderRegisterDataTransport.php
@@ -3,6 +3,16 @@
 namespace Bellisq\TypeMap\DI\Registers;
 
 
+/**
+ * [ Data Transport ] Provider Register Data Transport
+ * 
+ * Help ProviderRegister encapsulate data by constructor-injection.
+ * 
+ * @author 4kizuki <akizuki.c10.l65@gmail.com>
+ * @copyright 2017 Bellisq. All Rights Reserved.
+ * @package bellisq/type-map
+ * @since 1.0.0
+ */
 class ProviderRegisterDataTransport
 {
 
