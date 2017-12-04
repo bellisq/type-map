d241e52ec864676ce2c4e362a28227e5f6f6455a
4kizuki <akizuki.c10.l65@gmail.com>
2017-12-05 07:53:12 +0900

    Add phpDoc for CircularDependencyException

--- src/Exceptions/CircularDependencyException.php
+++ src/Exceptions/CircularDependencyException.php
@@ -5,6 +5,17 @@ namespace Bellisq\TypeMap\Exceptions;
 use LogicException;
 
 
+/**
+ * [ Exception ] Circular Dependency Found
+ * 
+ * Circular dependency is impossible to solve.
+ * Clarify and refactor dependency.
+ *
+ * @author 4kizuki <akizuki.c10.l65@gmail.com>
+ * @copyright 2017 Bellisq. All Rights Reserved.
+ * @package bellisq/type-map
+ * @since 1.0.0
+ */
 class CircularDependencyException extends LogicException
 {
 
