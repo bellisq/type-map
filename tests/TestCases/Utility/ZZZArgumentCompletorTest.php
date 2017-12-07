f28d57c38919c5583b7a6669815466b8057fc0d4
katayosegoodlife <katayose.goodlife@gmail.com>
2017-12-07 11:27:05 +0900

    increase test coverage

--- tests/TestCases/Utility/ZZZArgumentCompletorTest.php
+++ tests/TestCases/Utility/ZZZArgumentCompletorTest.php
@@ -2,8 +2,12 @@
 
 namespace Bellisq\TypeMap\Tests\TestCases\Utility;
 
+use Bellisq\TypeMap\Exceptions\InvalidArgumentDefinitionException;
+use Bellisq\TypeMap\Exceptions\UnsupportedArgumentTypeException;
+use Bellisq\TypeMap\Tests\Mocks\DIContainerTest\Objects\ZZZObjectA;
 use Bellisq\TypeMap\Tests\Mocks\ZZZSimpleInstantiator;
 use Bellisq\TypeMap\Tests\Mocks\ZZZSimpleInstantiatorClass;
+use Bellisq\TypeMap\Tests\Mocks\ZZZWrongInstantiatorClass;
 use Bellisq\TypeMap\Utility\ArgumentCompletor;
 use PHPUnit\Framework\TestCase;
 use ReflectionMethod;
@@ -22,6 +26,33 @@ class ZZZArgumentCompletorTest extends TestCase
     }
 
     public function t(ZZZSimpleInstantiatorClass $p)
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testInvalidArgumentDefinitionException()
+    {
+        $this->expectException(InvalidArgumentDefinitionException::class);
+        $x    = new ArgumentCompletor(new ZZZSimpleInstantiator());
+        $x->complete(new ReflectionMethod(ZZZArgumentCompletorTest::class, 'wrong1'));
+
+    }
+
+    public function wrong1($p)
+    {
+        $this->assertTrue(true);
+    }
+
+
+    public function testUnsupportedArgumentTypeException()
+    {
+        $this->expectException(UnsupportedArgumentTypeException::class);
+        $x    = new ArgumentCompletor(new ZZZSimpleInstantiator());
+        $x->complete(new ReflectionMethod(ZZZArgumentCompletorTest::class, 'wrong2'));
+
+    }
+
+    public function wrong2(ZZZWrongInstantiatorClass$p)
     {
         $this->assertTrue(true);
     }
